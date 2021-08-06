b9ad9f4526069ee8ba70924be4d1d72655cdc976
newb1er <taiwanmichael429@gmail.com>
2021-08-06 16:48:53 +0800

    Project Init Setup Files

---
+++ vue/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:vue/essential', 'airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {
+    'vue/html-self-closing': 'off',
+    'vue/no-v-model-argument': 'off',
+  },
+};
