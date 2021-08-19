983701ebe00b5afc9400f089bc7e2d3f9b6a1e35
newb1er <taiwanmichael429@gmail.com>
2021-08-19 15:30:02 +0800

    Mock Sync & Pass List to SyncIndicator Component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <TodoList />
+  <div>
+    <TodoList :list="list" />
+    <SyncIndicator :list="list" />
+  </div>
 </template>
 
 <script setup>
 import TodoList from './components/TodoList.vue';
+import SyncIndicator from './components/SyncIndicator.vue';
+
+const list = [];
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
---
+++ vue/src/components/SyncIndicator.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    id="sync-indicator"
+    @click="mockSyncing"
+    :class="[isLoading ? loadClass.loading : loadClass.done]"
+  ></div>
+</template>
+
+<script>
+export default {
+  props: ['list'],
+  data() {
+    return {
+      isLoading: false,
+      loadClass: {
+        loading: 'sync-indicator-loading',
+        done: 'sync-indicator-done',
+      },
+    };
+  },
+  methods: {
+    mockSyncing() {
+      this.isLoading = true;
+      console.log(this.list);
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2000);
+    },
+  },
+};
+</script>
+
+<style>
+#sync-indicator {
+  position: fixed;
+  top: 1.5em;
+  right: 1.5em;
+  width: 24px;
+  height: 24px;
+  padding: 0;
+}
+
+.sync-indicator-done {
+  background: url('../assets/cloud_done_black_24dp.svg');
+}
+
+.sync-indicator-loading {
+  background-image: url('../assets/sync_black_24dp.svg');
+  animation: spin 1.5s linear infinite;
+}
+@keyframes spin {
+  100% {
+    transform: rotate(-360deg);
+  }
+}
+</style>
